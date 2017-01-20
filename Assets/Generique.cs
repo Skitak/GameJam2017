45c3066069df2609acffbd05bba57f885b78a2d9
bas2205@live.fr <bas2205@live.fr>
2017-01-20 22:39:57 +0100

    ajout menu

---
+++ Assets/Generique.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Generique : MonoBehaviour {
+    public GameObject Buttons;
+    public GameObject EscapeObj;
+
+    // Use tis for initialization
+    void Start()
+    {
+        Buttons = GameObject.FindWithTag("Buttons");
+        EscapeObj = GameObject.FindWithTag("EscapeTag");
+        EscapeObj.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RunGeneric()
+    {
+        Buttons.SetActive(false);
+        EscapeObj.SetActive(true);
+    }
+}
---
+++ Assets/escape.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escape : MonoBehaviour {
+    public GameObject Buttons;
+    // Use this for initialization
+    void Start () {
+        Buttons = GameObject.FindWithTag("Buttons");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Escape))
+        {
+            Buttons.SetActive(true);
+            gameObject.SetActive(false);
+        }
+	}
+}
