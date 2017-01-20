67e5a7674eb511fee1d50ee24f20d1c66d0bfa1f
mathilde <mathilde.grapin@gmail.com>
2017-01-20 22:35:37 +0100

    modifs mathilde

--- Assets/Script/PointAndClick.cs
+++ Assets/Script/PointAndClick.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class PointAndClick : MonoBehaviour {
 
+    Vector3 positionClick;
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
+
+    /*
+    RaycastHit hit;
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    if(Physics.Raycast(ray, out hit)) {
+    positionClick = hit.point;
+    //transform.position = positionClick;
+    Debug.Log(positionClick);
+    }
+    }
+    return positionClick = new Vector3(0,0,0);
+    */
+    /*
+    void OnMouseUp() {
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+            
+    }
+    */
+    /*
+     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, 100))
+            print("Hit something!");
+    }
+    */
+
+    public void OnMouseDown()
+    {
+        print("down");
+    }
 }
