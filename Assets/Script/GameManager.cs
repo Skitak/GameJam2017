c02e95dafee98bec41faddbac6f7a6ad05be38ed
Bouquin Bastien <bas2205@live.fr>
2017-01-21 20:43:03 +0100

    team deathmatch added min ui too

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public GameObject[] players;
     public GameObject[] spawners;
     public float respawnTime;
+    public int nbKillRequired;
+    public float m_time_end;
+    public Text m_text;
 
+    private float timer;
     private static GameManager instance;
 
     void Awake()
@@ -18,13 +24,32 @@ public class GameManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        
+        timer = m_time_end;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+            end();
+        m_text.text = String.Format("{0:0}:{1:00}", Mathf.Floor(timer / 60), timer % 60); ;
 	}
+
+    void end()
+    {
+        GameObject winner = null;
+        int nbKill = 0;
+        foreach (GameObject gm in players)
+        {
+            if (gm.GetComponentInChildren<PlayerHead>().getNbKill() > nbKill)
+            {
+                nbKill = gm.GetComponentInChildren<PlayerHead>().getNbKill();
+                winner = gm;
+            }
+        }
+
+        won(winner.GetComponent<Player>());
+    }
 
     public static GameObject getBestSpawnPoint(GameObject playerOut)
     {
@@ -52,5 +77,15 @@ public class GameManager : MonoBehaviour {
         }
         return spawnPoint;
     }
-    
+
+    public static int getNbKillRequired()
+    {
+        return instance.nbKillRequired;
+    }
+
+    public static void won(Player p)
+    {
+        //TODO fin de partie
+    }
+
 } 
--- Assets/Script/PlayerHead.cs
+++ Assets/Script/PlayerHead.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHead : MonoBehaviour {
 
@@ -9,17 +11,20 @@ public class PlayerHead : MonoBehaviour {
 	public float timerBoostSpeed;
 	public float timerImmunity;
 	public shieldAnim shield;
+    public Text m_text;
 	private float startTime = 0.0f;
 	private bool isInvincible;
 
 	private float baseSpeed;
 	private float baseRotation;
+    private int nbKill = 0;
 
 	// Use this for initialization
 	void Start () {
 		dep = GetComponentInParent<Deplacement> ();
 		baseSpeed = dep.vMax;
 		baseRotation = dep.vRotate;
+        m_text.text = "0";
 	}
 
 	void Update () {
@@ -37,35 +42,50 @@ public class PlayerHead : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-		if (other.gameObject.CompareTag ("Player"))
-			other.gameObject.GetComponentInParent<Player> ().die ();
-		else if (other.gameObject.CompareTag ("Front")) {
-			shield.show ();
-			GetComponentInParent<Player> ().derive (other.gameObject);
-		}
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponentInParent<Player>().die();
+            ++nbKill;
+            m_text.text = nbKill.ToString();
+            if (nbKill > GameManager.getNbKillRequired())
+                GameManager.won(GetComponentInParent<Player>());
+        }
+        else if (other.gameObject.CompareTag("Front"))
+        {
+            shield.show();
+            GetComponentInParent<Player>().derive(other.gameObject);
+        }
         else if (other.gameObject.CompareTag("Wall"))
         {
             GetComponentInParent<Player>().die();
             //transform.parent.forward *= -1;
         }
-		else if(other.gameObject.CompareTag("Collectible")){
-			if (other.name == "collectibleVitesse") {
-				Destroy(other.gameObject);
-				dep.vMax = dep.vMax * coefBoostSpeed;
-				dep.vRotate = dep.vRotate * coefBoostSpeed;
-				Invoke ("SpeedTimer", timerBoostSpeed);
-			}
-			else if (other.name == "collectibleInvincible") {
-				Destroy (other.gameObject);
-				isInvincible = true;
-				Invoke ("ImmunityTimer", timerImmunity);
-			}
-		}
+        else if (other.gameObject.CompareTag("Collectible"))
+        {
+            if (other.name == "collectibleVitesse")
+            {
+                Destroy(other.gameObject);
+                dep.vMax = dep.vMax * coefBoostSpeed;
+                dep.vRotate = dep.vRotate * coefBoostSpeed;
+                Invoke("SpeedTimer", timerBoostSpeed);
+            }
+            else if (other.name == "collectibleInvincible")
+            {
+                Destroy(other.gameObject);
+                isInvincible = true;
+                Invoke("ImmunityTimer", timerImmunity);
+            }
+        }
         else
             GetComponentInParent<Player>().die();
     }
 
-	public bool getInvincible(){
+    public int getNbKill()
+    {
+        return nbKill;
+    }
+
+    public bool getInvincible(){
 		return isInvincible;
 	}
 
