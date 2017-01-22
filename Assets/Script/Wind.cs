983a0c51f802432d0a98656c02fefd4e478a2749
Bouquin Bastien <bas2205@live.fr>
2017-01-22 06:10:51 +0100

    Wind rewriten + feedback

--- Assets/Script/Wind.cs
+++ Assets/Script/Wind.cs
@@ -6,43 +6,92 @@ public class Wind : MonoBehaviour {
 
 	public float changeSpeed = 1;
     public GameObject[] winds;
-    private int activeWind;
+    private int activeWind = 0;
     public float timer_from;
     public float timer_to;
-    public float transition_time;
+    public float transition_time = 2;
     public GameObject particles;
 	public bool paused;
 
 	private float timer_begin;
-    private float timer;
+    private float timer = 0;
 
 	private Quaternion rotationOrigin;
 
+    public float feedbackTimeBeforeWindChange = 2;
+
+    public float feedbackDuration = 3;
+
+    public GameObject windFeedbackGo;
+
+    private Coroutine windChangeRoutine = null;
+
     void Start()
     {
-        changeDirection();
+
+        windFeedbackGo.GetComponent<MeshRenderer>().enabled = false;
+
+        activeWind = Random.Range(0, winds.Length);
+        timer = Random.Range(timer_from, timer_to);
+        timer_begin = timer;
+        rotationOrigin = transform.rotation;
+
+
     }
 
 	void Update () {
+
 		if (paused)
 			return;
+
+
         timer -= Time.deltaTime;
-        if (timer <= 0)
-            changeDirection();
 
-		//if (transform.rotation != winds [activeWind].transform.rotation)
-		transform.rotation = Quaternion.Lerp (rotationOrigin, winds [activeWind].transform.localRotation, 1-(timer / timer_begin));
-		//Debug.Log (timer / timer_begin);
+        if (timer <= feedbackTimeBeforeWindChange && windChangeRoutine == null)
+        {
+            windChangeRoutine = StartCoroutine("WindChange");
+           
+        }
 
-	}
-
-    private void changeDirection()
-    {
-        activeWind = Random.Range(0, winds.Length);
-        timer = Random.Range(timer_from, timer_to);
-		timer_begin = timer;
-        //Sound & animation
-		rotationOrigin = transform.rotation;
+	    
+		transform.rotation = Quaternion.Lerp (rotationOrigin, winds [activeWind].transform.localRotation, (timer_begin - timer) / 2);
 
     }
+
+    
+
+
+    IEnumerator WindChange() {
+
+
+        int nextActiveWind = Random.Range(0, winds.Length);
+
+        windFeedbackGo.transform.position = new Vector3(-Mathf.Sin(winds[nextActiveWind].transform.localEulerAngles.y * Mathf.Deg2Rad), 0, -Mathf.Cos(winds[nextActiveWind].transform.localEulerAngles.y * Mathf.Deg2Rad)) * 120;
+
+        if (nextActiveWind != activeWind)
+            windFeedbackGo.GetComponent<MeshRenderer>().enabled = true;
+
+
+        yield return new WaitForSeconds(feedbackTimeBeforeWindChange);
+
+
+        activeWind = nextActiveWind;
+        timer = Random.Range(timer_from, timer_to);
+        timer_begin = timer;
+        //Sound & animation
+        rotationOrigin = transform.rotation;
+
+
+
+        yield return new WaitForSeconds(feedbackDuration - feedbackTimeBeforeWindChange);
+
+        
+        windFeedbackGo.GetComponent<MeshRenderer>().enabled = false;
+
+        windChangeRoutine = null;
+
+        
+    }
+
+
 }
