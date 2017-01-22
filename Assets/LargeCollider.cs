ffe990e30288455a3878b72cf52b59c3a86b33de
lmoge2 <ludovicmoge@gmail.com>
2017-01-22 07:05:43 +0100

    First try for sound integration + update FMOD

---
+++ Assets/LargeCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeCollider : MonoBehaviour {
+
+    private PlayerSound audio;
+
+    void Start()
+    {
+        audio = GetComponent<PlayerSound>();
+    }
+
+    void OnTriggerEnter()
+    {
+        audio.OnContact();
+    }
+}
